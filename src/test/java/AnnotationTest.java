fd4196b2fc79f69ca721792e77f04c905d838268
crabapple <wishforyou.xia@gmail.com>
2019-11-17 05:04:29 +0800

    属性验证测试

--- src/test/java/AnnotationTest.java
+++ src/test/java/AnnotationTest.java
@@ -1,7 +1,4 @@
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.*;
 
 import cn.crabapples.annotations.Crabapples;
